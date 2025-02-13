2b11423dc3a84e44e5b5fb4ab871c23c1e69b513
Kye <kyecedar@proton.me>
2025-02-12 22:02:39 -0800

    yarr hello.

--- project/game/player/player.gd
+++ project/game/player/player.gd
@@ -10,30 +10,10 @@ extends VehicleBody3D
 
 #region // Variables.
 
-@export var MAX_STEER        : float = 45.0 ## Degrees.
-@export var WHEEL_TURN_SPEED : float =  1.0 ## 2.5 is really good handling, 1.5 is okay handling, 0.5 is shit handling.
-
-@export var BODY_LEAN_HELP_RATIO : float = 25.0 ## In degrees.
-
-@export_subgroup("Engine", "ENGINE_")
-
-@export var ENGINE_FORWARDS_POWER  : float = 15000.0
-@export var ENGINE_BACKWARDS_POWER : float = 10000.0
-
-@export_subgroup("Camera", "CAMERA_")
-
-@export var CAMERA_FOLLOW_AMOUNT      : float = 20.0
-@export var CAMERA_INTERPOLATE_AMOUNT : float =  5.0
-
-@export var CAMERA_LEAN_RATIO : float = 50.0 ## Is divided by local x linear_velocity.
-
-@export var CAMERA_FORWARD_MIN  : float = 5.0
-@export var CAMERA_BACKWARD_MIN : float = -6.0
-
-@export var CAMERA_MAX_ROLL_ROTATION : float = 30.0 ## In degrees.
-
 var forward_look_at : Vector3 = Vector3.ZERO
 var backward_look_at : Vector3 = Vector3.ZERO
+
+var miles_per_hour : float = 0.0
 
 #endregion Variables. ////////////////////
 
@@ -42,48 +22,57 @@ var backward_look_at : Vector3 = Vector3.ZERO
 #region // Functions.
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	forward_look_at = global_position
 	backward_look_at = global_position
 
 func _physics_process(delta: float) -> void:
-	steering = move_toward(steering, Input.get_axis("turn_right", "turn_left") * deg_to_rad(MAX_STEER), delta * WHEEL_TURN_SPEED)
-	var forwards_power : float = Input.get_action_strength("go_forwards") * ENGINE_FORWARDS_POWER
-	var backwards_power : float = -Input.get_action_strength("go_backwards") * ENGINE_BACKWARDS_POWER
-	engine_force = forwards_power + backwards_power
-	
+	process_go_go(delta)
 	process_body(delta)
 	process_camera(delta)
 
 
+func process_go_go(delta: float) -> void:
+	steering = move_toward(steering, Input.get_axis("turn_right", "turn_left") * deg_to_rad(Drive.PLAYER_MAX_STEER), delta * Drive.PLAYER_WHEEL_TURN_SPEED)
+	
+	var forwards_power : float = Input.get_action_strength("go_forwards") * Drive.PLAYER_ENGINE_FORWARDS_POWER
+	var backwards_power : float = -Input.get_action_strength("go_backwards") * Drive.PLAYER_ENGINE_BACKWARDS_POWER
+	
+	# enforce max speed by cutting engine power if it goes above max speed.
+	if miles_per_hour <= Drive.PLAYER_MAX_SPEED:
+		engine_force = forwards_power + backwards_power
+	else:
+		engine_force = 0.0
+
+
 func process_body(delta: float) -> void:
-	rotation.z = move_toward(rotation.z, clamp(rotation.z, -deg_to_rad(BODY_LEAN_HELP_RATIO), deg_to_rad(BODY_LEAN_HELP_RATIO)), delta * 1.0)
-	#rotation.z = move_toward(rotation.z, 0.0, delta * 50.0)
-	#rotation.x = move_toward(rotation.x, 0.0, delta * 50.0)
-	pass
+	# auto upright.
+	rotation.z = move_toward(rotation.z, clamp(rotation.z, -deg_to_rad(Drive.PLAYER_BODY_LEAN_HELP), deg_to_rad(Drive.PLAYER_BODY_LEAN_HELP)), delta * 1.0)
+
 
 func process_camera(delta: float) -> void:
-	%CameraPivot.global_position = %CameraPivot.global_position.lerp(global_position, delta * CAMERA_FOLLOW_AMOUNT)
-	%CameraPivot.transform = %CameraPivot.transform.interpolate_with(transform, delta * CAMERA_INTERPOLATE_AMOUNT)
+	%CameraPivot.global_position = %CameraPivot.global_position.lerp(global_position, delta * Drive.PLAYER_CAMERA_FOLLOW_AMOUNT)
+	%CameraPivot.transform = %CameraPivot.transform.interpolate_with(transform, delta * Drive.PLAYER_CAMERA_INTERPOLATE_AMOUNT)
 	
 	var z_dot : float = linear_velocity.dot(transform.basis.z)
 	var x_dot : float = linear_velocity.dot(transform.basis.x)
 	
+	# convert metres per second to miles per hour per eagle per football field.
+	miles_per_hour = abs(z_dot) * 2.236936
+	
 	# look in direction car is turning in.
 	# clamp value for forward to only be facing forward, opposite for back.
-	forward_look_at = forward_look_at.lerp(global_position + Vector3(max(z_dot, CAMERA_FORWARD_MIN), 0.0, max(z_dot, CAMERA_FORWARD_MIN)) * transform.basis.z, delta * CAMERA_INTERPOLATE_AMOUNT)
-	backward_look_at = backward_look_at.lerp(global_position + Vector3(min(z_dot, CAMERA_BACKWARD_MIN), 0.0, min(z_dot, CAMERA_BACKWARD_MIN)) * transform.basis.z, delta * CAMERA_INTERPOLATE_AMOUNT)
+	forward_look_at = forward_look_at.lerp(global_position + Vector3(max(z_dot, Drive.PLAYER_CAMERA_FORWARD_MIN), 0.0, max(z_dot, Drive.PLAYER_CAMERA_FORWARD_MIN)) * transform.basis.z, delta * Drive.PLAYER_CAMERA_INTERPOLATE_AMOUNT)
+	backward_look_at = backward_look_at.lerp(global_position + Vector3(min(z_dot, Drive.PLAYER_CAMERA_BACKWARD_MIN), 0.0, min(z_dot, Drive.PLAYER_CAMERA_BACKWARD_MIN)) * transform.basis.z, delta * Drive.PLAYER_CAMERA_INTERPOLATE_AMOUNT)
 	
 	%ForwardCamera.look_at(forward_look_at)
 	%BackwardCamera.look_at(backward_look_at)
 	
 	# lean camera into turns.
-	%ForwardCamera.rotate_z(x_dot / 50.0)
-	%BackwardCamera.rotate_z(-x_dot / 50.0)
+	if Drive.PLAYER_CAMERA_LEAN_RATIO:
+		%ForwardCamera.rotate_z(x_dot / Drive.PLAYER_CAMERA_LEAN_RATIO)
+		%BackwardCamera.rotate_z(-x_dot / Drive.PLAYER_CAMERA_LEAN_RATIO)
 	
-	%ForwardCamera.rotation.z = clamp(%ForwardCamera.rotation.z, -deg_to_rad(CAMERA_MAX_ROLL_ROTATION), deg_to_rad(CAMERA_MAX_ROLL_ROTATION))
-	
-	#print(x_dot)
+	%ForwardCamera.rotation.z = clamp(%ForwardCamera.rotation.z, -deg_to_rad(Drive.PLAYER_CAMERA_MAX_ROLL_ROTATION), deg_to_rad(Drive.PLAYER_CAMERA_MAX_ROLL_ROTATION))
 	
 	handle_camera_switch()
 
