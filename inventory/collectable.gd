1f99f0ce3c8f1377bf421508cecdaf98fc2742d0
Kye Cedar <kyecedar@proton.me>
2025-02-27 08:52:43 -0800

    no more mouse lock, removed redundant files.

--- inventory/collectable.gd
+++ inventory/collectable.gd
@@ -1,13 +1,28 @@
 extends Node3D
+
+
+
+#region // VARIABLES. //////////////////////////////////////////////////////////
+
 @onready var inventory_ui = $InventoryUI
 @export var item_name = ""
 @export var item_texture: Texture
+
+
 var scene_path: String = "res://inventory/Scenes/Inventory_Item.tscn"
+
+#endregion VARIABLES.
+
+
+
+#region // FUNCTIONS. //////////////////////////////////////////////////////////
+
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
 		print("got it")
 		pickup_item("apple")
 		queue_free()
+
 func pickup_item(item_name):
 	var item = {
 		"quantity": 1,
@@ -15,4 +30,7 @@ func pickup_item(item_name):
 		"texture": item_texture,
 		"scene_path": scene_path
 	}
+	
 	Global.add_item(item)
+
+#endregion FUNCTIONS.
