c59744bf01805055b32439135c30cf5563e2bc4d
plants212 <themrsnappy@gmail.com>
2025-03-10 16:39:39 +0400

    cool track
    please check out the enemy code I have an outline of what I want but i dummy thick

--- project/enemy/enemy.gd
+++ project/enemy/enemy.gd
@@ -2,19 +2,36 @@ extends CharacterBody3D
 
 var state_machine
 
-var SPEED = 100000
+var max_speed = 50
+var min_speed = 0
 var gravity = 1
+@onready var front = $front
 @onready var raycast = $RayCast3D
 @onready var nav = $NavigationAgent3D
 func update_target_location(target_location):
 	nav.set_target_position(target_location)
+	self.look_at(target_location)
 func _physics_process(delta):
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	else:
 		velocity.y -= 1
 	var current_location = global_transform.origin
 	var next_location = nav.get_next_path_position()
-	var new_velocity = (next_location-current_location).normalized() * SPEED
+	var new_velocity = (next_location-current_location).normalized() * max_speed
 	velocity = velocity.move_toward(new_velocity,0.25)
+	velocity.clampf(min_speed,max_speed)
+	print(new_velocity)
 	move_and_slide()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		pass
+		#	current speed *.85 for 5 seconds
+		#cum()
+	if body.is_in_group("enemy"):
+		pass
+		# current speed * 1.2 for 1 second
+		#stop cumming()
