81a0291c1d3851fff8862f5f6ba202e406d84772
plants212 <themrsnappy@gmail.com>
2025-03-24 18:56:54 +0400

    state machine and animation player
    added a state machine for detection and cooldown and an example of a detection animation

--- project/enemy/enemy.gd
+++ project/enemy/enemy.gd
@@ -1,11 +1,10 @@
 extends CharacterBody3D
 
 ## If the enemy will chase the player or not.
-@export var active : bool = true :
-	set(value):
-		active = value
-		
-		set_physics_process(active)
+enum States {IDLE, CHASE, COOLDOWN}
+
+# This variable keeps track of the character's current state.
+var state: States = States.IDLE
 
 
 @onready var e_collide : Timer = $enemy_collision
@@ -13,20 +12,13 @@ extends CharacterBody3D
 @onready var front : Node3D = $front
 @onready var raycast : RayCast3D = $RayCast3D
 @onready var nav : NavigationAgent3D = $NavigationAgent3D
-
+@onready var animation_player = $AnimationPlayer
+@onready var spotted_finished = false
+#@onready var enemy_body = $Area3D
 var state_machine
-var speed : float = 20.0
-var gravity : float = 1.0
+var speed : float = 8.0
+var gravity : float = 3.0
 var current_speed : float = 0.0
-
-var chase = false
-
-
-func _ready() -> void:
-	# 
-	active = active
-	pass
-
 
 func update_target_location(target_location: Vector3) -> void:
 	nav.set_target_position(target_location)
@@ -36,38 +28,64 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	else:
-		velocity.y -= 1.0
-	
-	update_target_location(Roadtrip.enemy_target_position)
-	
-	#if raycast.get_collider() is Player:
-		#chase = true
-	#if chase == true:
-	var z_dot : float = velocity.dot(transform.basis.z)
-	
-	current_speed = abs(z_dot) * 2.236936
-	
-	var current_location : Vector3 = global_transform.origin
-	var next_location : Vector3 = nav.get_next_path_position()
-	var new_velocity : Vector3 = (next_location-current_location).normalized() * speed
-	
-	velocity = velocity.move_toward(new_velocity,0.25)
-	
+		velocity.y -= 3.0
+	#STATE MACHEENE
+	if state == States.CHASE:
+		update_target_location(Roadtrip.enemy_target_position)
+		var z_dot : float = velocity.dot(transform.basis.z)
+		current_speed = abs(z_dot)
+		var current_location : Vector3 = global_transform.origin
+		var next_location : Vector3 = nav.get_next_path_position()
+		var new_velocity : Vector3 = (next_location-current_location).normalized() * speed
+		velocity = velocity.move_toward(new_velocity,0.05)
+		print(current_speed) 
+		move_and_slide()
+	if state == States.COOLDOWN:
+		update_target_location(Roadtrip.enemy_target_position)
+		var z_dot : float = velocity.dot(transform.basis.z)
+		var current_location : Vector3 = global_transform.origin
+		var next_location : Vector3 = nav.get_next_path_position()
+		var new_velocity : Vector3 = (next_location-current_location).normalized() * speed
+		velocity = velocity.move_toward(new_velocity,0.05)
+		speed =  -5
+	#	enemy_body.set_collision_layer(2)
+		move_and_slide()
 	move_and_slide()
+	
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
-		speed -= 22.0
+		state = States.COOLDOWN
 		p_collide.start()
 	
 	if body.is_in_group("enemy"):
-		speed += 5.0
+		speed += 1.0
 		e_collide.start()
 
-
 func _on_enemy_collision_timeout() -> void:
-	speed = 20.0
+	speed = 8
 
 func _on_player_collision_timeout() -> void:
-	speed = 20.0
+	speed = 8
+	state = States.CHASE
+	
+func set_state(new_state: int) -> void:
+	var previous_state := state
+	state = new_state
+
+	# You can check both the previous and the new state to determine what to do when the state changes. This checks the previous state.
+	if previous_state == States.IDLE:
+		pass
+	if previous_state == States.CHASE:
+		pass
+		#animation_player.play("cooldown")
+
+func _on_detection_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		animation_player.play("spotted_player")
+		state = States.CHASE
+
+
+func _on_animation_player_animation_finished(spotted_player) -> void:
+	spotted_finished = true
